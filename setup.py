59b95370f02a5b6eca5549588622a550650f604b
John Connolly <john.will.connolly@gmail.com>
2025-08-21 21:52:20 -0400

    moving to new flatbuffer usage

--- setup.py
+++ setup.py
@@ -6,14 +6,7 @@ import subprocess
 class BuildWithFlatc(build_py):
     def run(self):
         # Run flatc command before building
-        subprocess.check_call(["flatc", "--python", "schema_v3c.fbs"])
-        #move to quax
-        if os.path.exists("quax/tflite"):
-            subprocess.check_call(["rm", "quax/tflite", "-r"])
-        subprocess.check_call(["mv", "tflite", "quax"])
-        #give it an __init__.py
-        subprocess.check_call(["touch", "quax/tflite/__init__.py"])
-
+        subprocess.check_call(["flatc", "--filename-suffix", "_py_generated", "-o", "quax", "--python","--gen-onefile","--gen-object-api", "schemas/schema.fbs"])
         super().run()
 
 setup(
